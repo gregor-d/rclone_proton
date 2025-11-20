3659b0d370b8db2f865143ff6ca932dbd8f41613
Gregor D <33283245+gregor-d@users.noreply.github.com>
2025-11-21 00:12:59 +0100

    Remove --config option from rclone command

--- Synology rclone/rclone_backup/rclone_backup.sh
+++ Synology rclone/rclone_backup/rclone_backup.sh
@@ -24,8 +24,8 @@ touch "$LOGFILE"
 ART="========"
 echo "$ART Backup started at $(date '+%d.%m.%Y %T') $ART" | tee -a "$LOGFILE"
 
-# Build command
-RCLONE_CMD="rclone --config=\"$RCLONE_CONFIG\" copy \"$REMOTEDIR\" \"$LOCALDIR\" \
+# Build rclone command (no --config needed)
+RCLONE_CMD="rclone copy \"$REMOTEDIR\" \"$LOCALDIR\" \
     --log-level=$LOGLEVEL \
     --log-file=\"$LOGFILE\" \
     --transfers=$TRANSFER_LIMIT \
